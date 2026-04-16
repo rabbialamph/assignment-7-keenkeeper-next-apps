9b4e27d7f125f254ea8e656f7809e7dc3392f025
rabbialamph <mdrabbialom98@gmail.com>
2026-04-17 00:42:02 +0600

    context and hooks create and friends details create

---
+++ src/app/lib/providers/index.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import AppDataProvider from "../../../context/appContext";
+
+const Providers = ({ children }) => {
+    return (
+       <AppDataProvider>
+         {children}
+       </AppDataProvider>
+    );
+};
+
+export default Providers;
