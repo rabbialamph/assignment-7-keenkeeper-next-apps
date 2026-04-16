fbc319f0d2798a7bb7f00e37f1635c5274b263ea
rabbialamph <mdrabbialom98@gmail.com>
2026-04-17 00:52:01 +0600

    stats metadata add

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,6 +1,10 @@
-'use client'
+
 import Chart from "../../components/Chart/Chart";
 
+export const metadata = {
+  title: "Friendship Analytics | Keen Keeper",
+  description: "Friends and stats of the user",
+};
 
 
 const statPage = () => {
